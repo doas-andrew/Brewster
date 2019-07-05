4be6d7f9c3408a8b42f9571b4cdc6d102ea85569
Andrew Allen <asallen67@gmail.com>
2019-07-05 16:07:17 -0400

    seeds file updated

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,20 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # User.destroy_all
-Beer.destroy_all
 
-response = RestClient.get 'https://api.punkapi.com/v2/beers'
-response = JSON.parse(response)
-
-response.each { |beer|
-	Beer.create(
-		name: beer['name'], 
-		tagline: beer['tagline'],
-		description: beer['description'],
-		first_brewed: beer['first_brewed'],
-		image_url: beer['image_url'],
-		abv: beer['abv'],
-		ibu: beer['ibu'],
-		ph: beer['ph']
-	)
-}
+# only run this file once after migrating - this data should not change
+# require_relative './fetch_beers.rb'
