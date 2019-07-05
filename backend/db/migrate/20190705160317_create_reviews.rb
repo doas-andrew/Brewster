b5236fdf86e702cc21273a03ce823cc2b3dfa624
Andrew Allen <asallen67@gmail.com>
2019-07-05 16:50:11 -0400

    seeds.rb updated

--- backend/db/migrate/20190705160317_create_reviews.rb
+++ backend/db/migrate/20190705160317_create_reviews.rb
@@ -3,6 +3,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
     	t.string :title
     	t.string :content
+      t.integer :rating
     	
     	t.references :user
     	t.references :beer
