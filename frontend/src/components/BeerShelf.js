10cfec61eba37d248d918b5dd9cd4f86478607a5
Andrew Allen <asallen67@gmail.com>
2019-07-09 22:53:04 -0400

    big changes to BeerShelf

--- frontend/src/components/BeerShelf.js
+++ frontend/src/components/BeerShelf.js
@@ -5,27 +5,62 @@ import BeerSpecs from './BeerSpecs'
 class BeerShelf extends Component {
 
 	state = {
-		title: '',
-		showBeer: false
+		title: this.props.title,
+		showBeer: false,
+		showRevs: false
 	}
 
-	// beers = this.props.beers ? this.props.beers : []
+	submitReview = e => {
+		e.preventDefault()
+		let form = e.target
 
-	componentDidMount(){
-		this.setState({title: this.props.title})
+		fetch('http://localhost:3000/reviews',{
+			method: 'POST',
+			headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				title: this.state.showBeer.name,
+				content: form.content.value,
+				rating: form.rating.value,
+				user_id: localStorage.getItem('brewster_id'),
+				beer_id: this.state.showBeer.id
+			})
+		})
+		.then(res => this.getShowBeer())
+		form.reset()
 	}
 
-	changeShowBeer = (beer)=> {
-		fetch('http://localhost:3000/beers/' + beer.id)
+	checkFav = () => !!this.state.showBeer.favorites.find(fav => fav.user_id == localStorage.getItem('brewster_id'))
+
+	handleFav = () => {
+		let url= 'http://localhost:3000/favorites/'
+		let userId = localStorage.getItem('brewster_id')
+		let fetchHash = {}
+
+		if(this.checkFav()) {
+			url += this.state.showBeer.favorites.find(fav => fav.user_id == userId).id
+			fetchHash.method = 'DELETE'
+		}
+		else {
+			fetchHash.method = 'POST'
+			fetchHash.headers = { Accept: 'application/json', 'Content-Type': 'application/json' }
+			fetchHash.body = JSON.stringify({ user_id: userId, beer_id: this.state.showBeer.id })
+		}
+
+		fetch(url, fetchHash)
+		.then(res => this.getShowBeer())
+	}
+
+	getShowBeer = (id=this.state.showBeer.id)=> {
+		return fetch('http://localhost:3000/beers/'+id)
 		.then(res => res.json())
-		.then(res => this.setState({ showBeer: res, title: beer.name }) )
+		.then(res => this.setState({  title: res.name, showBeer: res, showRevs: res.reviews }) )
 	}
 
 	renderBeers = ()=> {
 		return (
 			<div className="beer-scroll-grid">
 				{this.props.beers.map( beer =>
-					<div className="item" onClick={ e => this.changeShowBeer(beer)} style={{ backgroundImage: `url(${beer.image_url})` }} >
+					<div className="item" onClick={ e => this.getShowBeer(beer.id)} style={{ backgroundImage: `url(${beer.image_url})` }} >
 						<h5>{this.checkBeerName(beer.name)}</h5>
 					</div>
 				)}
@@ -43,23 +78,30 @@ class BeerShelf extends Component {
 
 	closeBeerSpecs = ()=> {
 		this.setState({
+			title: this.props.title,
 			showBeer: false,
-			title: this.props.title
+			showRevs: false
 		})
 	}
 
+	renderReviewList = ()=>
+		<div className="review-list">
+			<h4>Reviews</h4>
+			<ul style={{ listDecoration: 'none' }}>
+				{ this.state.showBeer.reviews.map(review => <li>{review.content} <button>x</button></li>) }
+			</ul>
+		</div>
+
 	render() {
 		return (
-			<div className="beer-shelf" style={{ height: this.state.showBeer ? 'auto' : "420px", paddingBottom: this.state.showBeer ? '1em' : "0" }}>
-				<h3>{this.state.title}</h3>
-				<hr/>
-				{
-					this.state.showBeer ? 
-						<BeerSpecs closeBeerSpecs={this.closeBeerSpecs} beer={this.state.showBeer} />
-						:
-						this.renderBeers()
-				}
-		  </div>
+			<Fragment>
+				<div className="beer-shelf">
+					<h3>{this.state.title}</h3>
+					<hr/>
+					{ this.state.showBeer ? <BeerSpecs beer={this.state.showBeer} submitReview={this.submitReview} checkFav={this.checkFav} handleFav={this.handleFav} closeBeerSpecs={this.closeBeerSpecs} /> : this.renderBeers() }
+			  </div>
+			  { this.state.showBeer ? this.renderReviewList() : null }
+			</Fragment>
 		)
 	}
 }
--- frontend/src/components/BeerSpecs.js
+++ frontend/src/components/BeerSpecs.js
@@ -6,78 +6,16 @@ const loggedIn = !!localStorage.getItem('brewster_token')
 
 class BeerSpecs extends Component {
 
-	state = {
-		beer: this.props.beer,
-		rev: false,
-		review: '',
-		rating: 1
-	}
-
-	fetchBeerState = ()=> {
-		fetch('http://localhost:3000/beers/'+this.state.beer.id)
-		.then(res => res.json())
-		.then(res => this.setState({ beer: res }) )
-	}
-
-	handleFav = () => {
-		let url= 'http://localhost:3000/favorites/'
-		let c_user_id = localStorage.getItem('brewster_id')
-		let fetchHash = {}
-
-		if(this.checkFav()) {
-			url += this.state.beer.favorites.find(fav => fav.user_id == c_user_id).id
-			fetchHash.method = 'DELETE'
-		}
-		else {
-			fetchHash.method = 'POST'
-			fetchHash.headers = { Accept: 'application/json', 'Content-Type': 'application/json' }
-			fetchHash.body = JSON.stringify({ user_id: c_user_id, beer_id: this.state.beer.id })
-		}
-		fetch(url, fetchHash)
-		.then(res => this.fetchBeerState() )
-	}
-
-	handleRev = () => {
-		console.log('i have been touched!')
-		this.setState({ rev: !this.state.rev })
-	}
-
-	checkFav = () => {
-		return !!this.state.beer.favorites.find(fav => fav.user_id == localStorage.getItem('brewster_id'))
-	}
-	handleChange = (e) => {
-		let input = e.target.value
-		this.setState({ review: input})
-	}
-
-	handleSubmit = (e) => {
-		e.preventDefault()
-		let form = e.target
-		fetch('http://localhost:3000/reviews',{
-		method: 'POST',
-		headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
-		body: JSON.stringify({
-			title: this.state.beer.name,
-			content: form.content.value,
-			rating: form.rating.value,
-			user_id: localStorage.getItem('brewster_id'),
-			beer_id: this.state.beer.id
-		})
-	})
-	.then(e.target.reset())
-	.then(e => this.setState({ rev: !this.state.rev}))
-	.then(fetch('http://localhost:3000/beers/' + this.state.beer.id)
-	.then(res => res.json())
-	.then(res => this.setState({ beer: res }))
-	)
-	}
+	state = { showReviewForm: false }
 
 	renderReviewForm = ()=>
-		<form id='showBeer-review' onSubmit={this.handleSubmit}>
+		<form id='showBeer-review' onSubmit={this.props.submitReview}>
 			<strong>Leave a Review!</strong>
-			<textarea id='showBeer-review-input' rows='5' cols='25' onChange={this.handleChange} name="content" placeholder=' Type your review here'></textarea>
-			<br/>
-			&nbsp; &nbsp; <strong>Rating </strong> &nbsp;
+			<textarea id='showBeer-review-input' rows='5' cols='25' name="content" placeholder=' Type your review here'></textarea>
+			
+			<br/> &nbsp; &nbsp;
+
+			<strong>Rating </strong> &nbsp;
 			<select name="rating">
 				<option value='1'>1</option>
 				<option value='2'>2</option>
@@ -85,18 +23,11 @@ class BeerSpecs extends Component {
 				<option value='4'>4</option>
 				<option value='5'>5</option>
 			</select>
+
 			&nbsp; &nbsp; &nbsp;
+
 			<Button type='submit' variant="secondary">Submit</Button>
 		</form>
-
-	renderReviewList = ()=>
-		<div>
-			<hr/>
-			<h4>Reviews</h4>
-			<ul>
-				{ this.state.beer.reviews.map(review => <li>{review.content}<button>X</button></li>) }
-			</ul>
-		</div>
 
 	render() {
 		return (
@@ -110,36 +41,37 @@ class BeerSpecs extends Component {
 
 						<br/><br/>
 
-						<span id='heart' style={{ color: this.checkFav() ? 'red' : 'grey' }} onClick={ ()=> loggedIn ? this.handleFav() : null }>
+						<span id='heart' style={{ color: this.props.checkFav() ? 'red' : 'grey' }} onClick={ ()=> loggedIn ? this.props.handleFav() : null }>
 							<FaHeart/>
-						</span> {this.state.beer.favorites.length}
+						</span> {this.props.beer.favorites.length}
+
 						&nbsp; &nbsp; &nbsp;
-						<span id='reviews' style={{ color: this.state.rev ? 'DodgerBlue' : 'grey' }} onClick={ e => loggedIn ? this.setState({ rev: !this.state.rev }) : null }>
+
+						<span id='reviews' style={{ color: this.state.showReviewForm ? 'DodgerBlue' : 'grey' }} onClick={ e => loggedIn ? this.setState({ showReviewForm: !this.state.showReviewForm }) : null }>
 							<FaEdit/>
-						</span> {this.state.beer.reviews.length}
+						</span> {this.props.beer.reviews.length}
 						
 						<br/><br/>
 
 						<span id="close-showBeer" onClick={this.props.closeBeerSpecs}><FaChevronCircleLeft />&nbsp; Back</span>
 					</div>
 
-					<div className="col item" style={{ backgroundImage: `url(${this.state.beer.image_url})` }} ></div>
+					<div className="col item" style={{ backgroundImage: `url(${this.props.beer.image_url})` }} ></div>
 
 					<div id="showBeer-values" className="col-5">
 						<div className="row">
 							<div className="col-3">
-								<strong>{this.state.beer.abv}</strong><br/>
-								<strong>{this.state.beer.ibu}</strong><br/>
-								<strong>{this.state.beer.ph}</strong><br/>
+								<strong>{this.props.beer.abv}</strong><br/>
+								<strong>{this.props.beer.ibu}</strong><br/>
+								<strong>{this.props.beer.ph}</strong><br/>
 							</div>
 
 							<div className="col">
-								{ this.state.rev && loggedIn ? this.renderReviewForm() : null }
+								{ loggedIn && this.state.showReviewForm ? this.renderReviewForm() : null }
 							</div>
 						</div>
 					</div>
 				</div>
-					{ this.state.beer.reviews[0] ? this.renderReviewList() : null }
 			</div>
 		)
 	}
